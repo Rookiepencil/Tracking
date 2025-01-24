90440d983e56e451062a2bc9a1e5daeccb5cf7b7
Henry <Henry@DESKTOP-5HI2U8Q.home>
2025-01-24 16:22:28 -0500

    改了一下movetrarget的内容

--- Assignment1_Cloud/main.m
+++ Assignment1_Cloud/main.m
@@ -4,17 +4,19 @@ close all
 
 p = parameters();
 Sensor_Parameter = p.sensor;
-figure; % open the figure and make sure it can always update on that graph
-hold on; 
-xlabel('X Position');
-ylabel('Y Position');
-title('Assignment 1');
-grid on;
+dt = p.target(1).sampletime; % Sampling time
+Target_Process_Noise = p.target(1).process_noise;
 
 %% Main Loop
 for r=1:p.scenario.monte_runs
+     figure; % open the figure and make sure it can always update on that graph
+     hold on; 
+     xlabel('X Position');
+     ylabel('Y Position');
+     title('Assignment 1');
+     grid on;
     %%% perform any initialization this run
-
+    target_Number = 0;
     xk_1 = p.target(1).start_state; % Initial state of the target
     plot(xk_1(1), xk_1(3), 'b.', 'MarkerSize', 15); % Plot target Movement
     plot(Sensor_Parameter.Xpos, Sensor_Parameter.Ypos, 'g.', 'MarkerSize', 25); % Plot target Movement
@@ -25,14 +27,12 @@ for r=1:p.scenario.monte_runs
         %plot(MeasurementConvert(1), MeasurementConvert(2), 'm.', 'MarkerSize', 15); 
     end
     
-    dt = p.target(1).sampletime; % Sampling time
-    Parameter = p.target(1).process_noise;
-
     for k=1:p.scenario.num_of_time_steps 
-        T = k + dt;
-        [xk, Q] = moveTarget(Parameter, xk_1, dt);
+
+        [xk, Q] = moveTarget(Target_Process_Noise, xk_1, dt);
         xk_1 = xk;
         plot(xk(1), xk(3), 'b.', 'MarkerSize', 15); % Plot target Movement
+        target_Number = target_Number + 1;
         [Measurement, FalseAlarm]= generateMeasurements(Sensor_Parameter, xk);
         if isempty(Measurement)
             continue;
@@ -45,13 +45,13 @@ for r=1:p.scenario.monte_runs
         % .. = dataAssociation(....);
         % 
         % .. = kalmanFilter(....)
-        
+        %pause(dt)    
     end
     
     hold off
     
+    
 end
-
 %% calculate the RMSE
 
 
--- Assignment1_Cloud/moveTarget.m
+++ Assignment1_Cloud/moveTarget.m
@@ -1,4 +1,4 @@
-function [xk,Q] = moveTarget(parameter, xk_1, T) % add additional parameters, if necessary
+function [xk,Q] = moveTarget(Process_Noise, xk_1, T) % add additional parameters, if necessary
     F = [1, T, 0, 0;
          0, 1, 0, 0;
          0, 0, 1, T;
@@ -6,7 +6,7 @@ function [xk,Q] = moveTarget(parameter, xk_1, T) % add additional parameters, if
         ];
 
     G = [(T^2 / 2), 0; 0, T; (T^2 / 2), 0; 0, T];
-
-    xk = F * xk_1 + G * parameter *randn(2,1);
-    Q  = G * parameter * G';
+    RandomNoise = (G * sqrt(Process_Noise) *randn(2,1));
+    xk = F * xk_1+ RandomNoise;
+    Q  = G * Process_Noise * G';
 end
